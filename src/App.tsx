589f830759ce2567e8f18315c34412ab7f3db4d5
Samuel Pasieka <samuel-p@outlook.de>
2022-09-25 22:35:10 +0200

    Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Board mines={50} height={16} width={24}/>
+        <Board mines={99} height={16} width={30}/>
       </header>
 
     </div>
